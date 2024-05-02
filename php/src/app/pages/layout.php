cc133299e9315b2615ffb48598397df26b640381
Vsevolod Fadeev <sevafad@gmail.com>
2024-05-02 19:12:54 +0300

    Folder structure changes.

--- php/src/app/pages/layout.php
+++ php/src/app/pages/layout.php
@@ -1,3 +1,7 @@
+<?php 
+$pageName = str_replace('.php', '', $_SERVER['PHP_SELF']);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,8 +9,8 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="/app/shared/ui/layout.css" />
-  <link rel="stylesheet" href="<?php echo "/app/pages" . str_replace('.php', '.css', $_SERVER['PHP_SELF']); ?>" />
+  <link rel="stylesheet" href="/app/pages/layout.css" />
+  <link rel="stylesheet" href="<?php echo "/app/pages" . $pageName . $pageName . '.css';?>" />
   <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
   <title>Document</title>
 </head>
@@ -15,7 +19,7 @@
   <header></header>
   <main class="main">
     <?php
-    include_once dirname(__FILE__) . '/../../pages' . $_SERVER['PHP_SELF']; 
+    include_once dirname(__FILE__) . '/' . $pageName . $pageName . '.php';
     ?>
   </main>
 </body>
