da8c6acf162f737ebc598f669e29b379963a0c00
Vsevolod Fadeev <sevafad@gmail.com>
2024-05-20 00:41:22 +0300

    Create employer vacancies list.

--- php/src/app/pages/employer-vacancies/employer-vacancies.php
+++ php/src/app/pages/employer-vacancies/employer-vacancies.php
@@ -1,1 +1,18 @@
-Hello World
+<?php
+$vacancies = $getEmployerVacancies->execute();
+?>
+
+<div class='list'>
+  <?php foreach ($vacancies as $vacancy): ?>
+  <div data-id="<?= $vacancy->id ?>" class='vacancy item item_hoverable _border_sub'>
+    <h3 class='item__title'><?= $vacancy->title ?></h3>
+  </div>
+  <?php endforeach; ?>
+</div>
+
+<script>
+$('.vacancy').on('click', function() {
+  const vacancyId = $(this).data('id');
+  window.location.href = '/employer-vacancy.php?id=' + vacancyId;
+});
+</script>
